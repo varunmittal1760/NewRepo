a067021178bc45d53ebef0785221da5ea5277d34
Varun <varunmittal155@gmail.com>
2023-06-20 12:27:05 +0100

    Add project files.

---
+++ SignalRProject/Hubs/TASSignalRHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+    public class TASSignalRHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
